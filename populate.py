6c13a6e323808ad90e332170d0c5beb0ce094990
Denis <tribushkov@gmail.com>
2015-10-21 15:08:03 +0300

    some refactoring

--- populate.py
+++ populate.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 import os
-from random import randint
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'board.settings')
-
-from datetime import datetime
 import django
+from random import randint
+from app.models import *
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'board.settings')
 django.setup()
 
-from app.models import *
 
 cities = [
     'Москва',
@@ -23,8 +22,8 @@ cities = [
 
 
 def populate():
-    for day in range(1,31):
-        for i in range (1,15):
+    for day in range(1, 31):
+        for i in range(1, 15):
             start_hour = randint(6, 11)
             stop_hour = randint(12, 18)
             start_min = randint(1, 59)
@@ -33,23 +32,23 @@ def populate():
             start_time = datetime.strptime(start_time, '%X').time()
             end_time = str(stop_hour) + ':' + str(stop_min) + ':00'
             end_time = datetime.strptime(end_time, '%X').time()
-            date = '2015-10-' + str(day)
+            trip_date = '2015-10-' + str(day)
             for start_city in cities:
                 start_city = start_city.decode('utf-8').upper()
                 for stop_city in cities:
                     seats = randint(0, 10)
                     stop_city = stop_city.decode('utf-8').upper()
-                    add_trip(start_city, stop_city, start_time, end_time, date, seats)
+                    add_trip(start_city, stop_city, start_time, end_time, trip_date, seats)
 
 
-def add_trip(start_point, end_point, start_time, end_time, date, seats):
+def add_trip(start_point, end_point, start_time, end_time, trip_date, seats):
     t = Trip(
-        start_point = start_point,
-        end_point = end_point,
-        start_time = start_time,
-        end_time = end_time,
-        date = date,
-        seats = seats
+        start_point=start_point,
+        end_point=end_point,
+        start_time=start_time,
+        end_time=end_time,
+        date=trip_date,
+        seats=seats
     )
     t.save()
     return t
